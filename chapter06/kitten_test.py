8f313d161bafbab40029becf159acf9b73e01618
Vabysa <164088880+Vabysa@users.noreply.github.com>
2024-05-19 15:17:04 +0330

    new save

--- chapter06/kitten_test.py
+++ chapter06/kitten_test.py
@@ -8,3 +8,9 @@ for attempt in range(1, 5):
     response = ab.open('http://kittenwar.com')
     for cookie in ab.cookie_jar:
         print(cookie)
+        for attempt in range(1, 5):
+    ab.anonymize()
+    print('[*] Fetching page')
+    response = ab.open('http://kittenwar.com')
+    for cookie in ab.cookie_jar:
+        print(cookie)
